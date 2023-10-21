2e72d92c1ecc890cfe34e8a406bb91bb9924ee4f
Sidric <sid25ric@gmail.com>
2023-10-21 17:44:22 -0400

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Automatically detect and classify defects in gears',
+    author='AI_Gear_analysis_project_2023',
+    license='MIT',
+)
